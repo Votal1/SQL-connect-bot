071feaf2891c93ee6d0848747c27ee3d3be1d28d
Votal1 <vitaliypankiv1@gmail.com>
2022-02-01 10:01:34 +0200

    first commit

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import telebot
+from telebot import types
 
+TOKEN = '5212114227:AAFiX_WnuBX9pxm3xlDZtuujJhVN3ukoHzo'
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hi')
+
+
+bot.polling()
