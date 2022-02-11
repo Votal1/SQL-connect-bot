b3a201021815bba24418e0be5121577a03356ec0
Vitalii <60795197+Votal1@users.noreply.github.com>
2022-02-12 00:35:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, 'Hello world112')
+    bot.reply_to(message, 'Hello world1123')
 
 
 bot.polling(none_stop=True)
