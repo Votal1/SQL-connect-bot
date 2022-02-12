abbbceed0f81192b790be0e807a87034c83007bd
Votal1 <vitaliypankiv1@gmail.com>
2022-02-12 21:52:14 +0200

    add terraform

---
+++ terraform/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_security_group" "tf-sg" {
+  name = "tf-sg"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "deploy_server" {
+  ami           = "ami-0d527b8c289b4af7f"
+  instance_type = "t2.micro"
+  key_name      = "jenkins"
+  vpc_security_group_ids = [aws_security_group.tf-sg.id]
+
+  tags = {
+    Name = "DeployServer"
+  }
+}
