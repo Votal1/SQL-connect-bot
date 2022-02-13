9893d0fa38080d44bf3d7b4663f7fba1614500c8
Vitalii <60795197+Votal1@users.noreply.github.com>
2022-02-13 17:06:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,18 @@
 from os import environ
 import telebot
 from telebot import types
+import redis
 
 TOKEN = environ.get('TOKEN')
 bot = telebot.TeleBot(TOKEN)
 
+r = redis.Redis(host=environ.get('REDIS_HOST'), port=16801,
+                password=environ.get('REDIS_PASSWORD'), db=0)
+
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, 'Hello world1123')
+    bot.reply_to(message, 'Hello world1123' + str(r.keys()))
 
 
 bot.polling(none_stop=True)
