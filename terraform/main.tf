0a07b3786907c76cbb31993242302c8af7f7b46d
Vitalii <60795197+Votal1@users.noreply.github.com>
2022-05-26 15:38:51 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,9 +10,9 @@ terraform {
 }
 
 provider "aws" {
-  region     = "us-east-1"
-  access_key = "AKIAT2U3KSJOODOWKUF2"
-  secret_key = "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"
+  region     = "eu-central-1"
+  access_key = "AKIA52X6EA3KVEXDSPYJ"
+  secret_key = "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"
 }
 
 resource "aws_security_group" "terraform-sg" {
@@ -35,7 +35,7 @@ resource "aws_security_group" "terraform-sg" {
 resource "aws_instance" "deploy_server" {
   ami           = "ami-0d527b8c289b4af7f"
   instance_type = "t2.micro"
-  key_name      = "jenkins"
+  key_name      = "jk"
   vpc_security_group_ids = [aws_security_group.terraform-sg.id]
 
   tags = {
